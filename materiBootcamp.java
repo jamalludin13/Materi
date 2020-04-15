9475a3b97adfe4eefada265712a77de440e53719
Jamaludin <61264944+jamalludin13@users.noreply.github.com>
2020-04-15 17:52:38 +0700

    Add files via upload

---
+++ materiBootcamp.java
@@ -0,0 +1,21 @@
+public class materiBootcamp {
+
+    public static void main(String[] args) { 
+	
+        System.out.println("--------- Materi selama Bootcamp-------");
+		
+        System.out.println("Minggu ke 1 :");
+		System.out.println(" 1. Pengenalan Java  \n 2. Java OOP ");
+		
+		System.out.println("Minggu ke 2 :");
+		System.out.println(" 1. J2SE \n 2. J2EE ");
+		
+		System.out.println("Minggu ke 3 :");
+		System.out.println(" 1. Pengenalan Framework \n 2. Framework Spring ");
+		
+		  
+		
+		
+
+    }
+}
